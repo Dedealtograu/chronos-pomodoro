1398d768681da5d14f582051c686bcdb882291a8
Dedealtograu <matias.jms14@gmail.com>
2025-03-28 17:53:26 -0300

    Final da aula 17

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
-import './styles/theme.css';
-import './styles/global.css';
-import { Heading } from './components/Heading';
+import './styles/theme.css'
+import './styles/global.css'
+import { Heading } from './components/Heading'
+import { TimerIcon } from 'lucide-react'
 
 export function App() {
   return (
     <>
-      <Heading />
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. A dolore, voluptates eius consequuntur cupiditate earum quod perspiciatis suscipit, nisi error numquam doloremque ea minima. Molestiae eos beatae ea quam voluptatum.</p>
+      <Heading>
+        Olá Mundo 1
+        <button>
+          <TimerIcon />
+        </button>
+      </Heading>
+
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. A dolore, voluptates eius consequuntur cupiditate earum
+        quod perspiciatis suscipit, nisi error numquam doloremque ea minima. Molestiae eos beatae ea quam voluptatum.
+      </p>
     </>
   )
 }

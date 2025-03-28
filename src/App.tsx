cd831d5bb1a379470dfd001cbbde0bd8aeeb9ce0
Dedealtograu <matias.jms14@gmail.com>
2025-03-28 12:30:39 -0300

    Commit inicial

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './styles/theme.css';
+import './styles/global.css';
+import { Heading } from './components/Heading';
+
+export function App() {
+  return (
+    <>
+      <Heading />
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. A dolore, voluptates eius consequuntur cupiditate earum quod perspiciatis suscipit, nisi error numquam doloremque ea minima. Molestiae eos beatae ea quam voluptatum.</p>
+    </>
+  )
+}

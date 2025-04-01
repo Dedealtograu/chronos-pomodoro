ce7ba31af79b8d51b66609af95b74a010d21bfe8
Dedealtograu <matias.jms14@gmail.com>
2025-04-01 13:41:20 -0300

    Final da aula 27

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import './styles/theme.css'
 import './styles/global.css'
-import { Heading } from './components/Heading'
 import { Container } from './components/Container'
 import { Logo } from './components/Logo'
+import { Menu } from './components/Menu'
+import { CountDown } from './components/CountDown'
+import { DefaultInput } from './components/DefaultInput'
 
 export function App() {
   return (
@@ -12,7 +14,29 @@ export function App() {
       </Container>
 
       <Container>
-        <Heading>MENU</Heading>
+        <Menu />
+      </Container>
+
+      <Container>
+        <CountDown />
+      </Container>
+
+      <Container>
+        <form action='' className='form'>
+          <div className='formRow'>
+            <DefaultInput labelText='task' id='input' type='text' />
+          </div>
+          <div className='formRow'>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+          <div className='formRow'>
+            <p>Ciclos</p>
+            <p>0 0 0 0 0 0</p>
+          </div>
+          <div className='formRow'>
+            <button>Enviar</button>
+          </div>
+        </form>
       </Container>
     </>
   )
